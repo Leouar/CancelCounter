817bcbaf323871bc0d219b4ef17e0fd3bac4dd4b
Simon Attila <simonattila790@gmail.com>
2024-06-01 17:21:32 +0200

    New Command gibplatz -> Searches the user at the provided Rank in the Leadboard and Prints it

---
+++ commands/gibplatz.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('gibplatz')
+		.setDescription('Gibt den User an der gegebenen Stelle im Leaderboard aus!')
+        .addIntegerOption(option => 
+            option
+                .setName("platz")
+                .setDescription("Gib den Platz im Leaderboard an den du Abfragen willst")
+                .setRequired(true)),
+
+	async execute(interaction) {
+        const amount = interaction.options.getInteger("platz") - 1;
+        const userIds = [...cancelMap.keys()];
+        userIds.sort((a,b) => {return cancelMap.get(b) - cancelMap.get(a)});
+        console.log(userIds[amount]);
+        await interaction.reply(`Den ${amount + 1}. belegt ${(await interaction.guild.members.fetch(userIds[amount])).displayName} und wurde ${cancelMap.get(userIds[amount])} mal gecancelt!`);
+       
+	},
+};
